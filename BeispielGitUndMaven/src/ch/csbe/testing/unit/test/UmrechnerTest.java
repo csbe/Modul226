9ff09ad6f8719e35fc2742687c1d8210c42955f1
Enrico Buchs <enrico.buchs@schule.bern.local>
2018-03-22 20:18:46 +0100

    add new comment

--- BeispielGitUndMaven/src/ch/csbe/testing/unit/test/UmrechnerTest.java
+++ BeispielGitUndMaven/src/ch/csbe/testing/unit/test/UmrechnerTest.java
@@ -11,6 +11,7 @@ public class UmrechnerTest {
 	@Test
 	public void testEurToCHF() {
 		//109.8901...
+		//weiterer kommentar inline
 		assertEquals(109.8901, WaehrungRechner.EurToCHF(100), 0.0001);
 	}
 
